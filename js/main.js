fcd10c7228de05e0a5816d99176598ff9c63417a
Ryan Fleck <ryan.fleck@protonmail.com>
2018-10-16 16:57:35 -0400

    Add XML data to index.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
+console.log("OC-Transpo load/parse XML experiment.");
 
+function parseData(xmlID){
+    var insertPoint = document.getElementById("oct-updates");
+    var octInput = document.getElementById(xmlID);
+    var events = octInput.getElementsByTagName('item');
+
+    for(var i=0;i<events.length;i++){
+        //console.log(events[i]);
+        var title = getFirstNodeData(events[i],'title');
+        console.log("Processing "+title);
+
+        var container = document.createElement('article');
+        container.appendChild(document.createTextNode(title));
+
+        insertPoint.appendChild(container);
+
+    }
+
+
+    //insertPoint.appendChild("test");
+}
+
+function main(){
+    parseData("oct-data-en"); //rss/channel/item
+    console.log("Done.");
+}
+
+function getFirstNodeData(node, tag){
+    return node.getElementsByTagName(tag)[0].firstChild.data;
+}
+
+/*
+
+// Cannot load external XML, especially while loading index as a file.
+function loadOCTData(){
+
+    language = (lang)? lang : "en";
+    console.log("Loading "+language);
+
+    var OCT = new XMLHttpRequest();
+    OCT.addEventListener("load", processOCTData);
+    OCT.open('GET',('./data/oct-fr.xml'),true);
+    OCT.send(null);
+}
+
+function processOCTData(){
+    console.log("Processing OCT Data...");
+}
+
+
+
+*/
+
+window.addEventListener('load',main);
